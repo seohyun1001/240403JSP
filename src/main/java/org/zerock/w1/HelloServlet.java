3bd453c18a6b3a774923fec1de49a6f1d055c8a0
Hyoeun01 <shyoeun09@gmail.com>
2024-04-03 11:51:02 +0900

    240403 3교시

--- src/main/java/org/zerock/w1/HelloServlet.java
+++ src/main/java/org/zerock/w1/HelloServlet.java
@@ -8,9 +8,12 @@ import javax.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     private String message;
 
+
+    // 초기화 담당. helloservlet 호출시 제일먼저 실행됨
     public void init() {
         message = "Hello World!";
     }
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
